08b1250491f7fd7f66796af5e2ab227e9adf97ca
JoaquinP15 <94157179+JoaquinP15@users.noreply.github.com>
2022-09-11 11:42:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+Status = "";
+value = "";
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    object_detector =  ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = 'status : detecting objects';
+    value = document.getElementById("input").value;
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    Status = true;
+}
+
+function gotResults(error,results){
+    if(error){
+       console.log(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(video,0,0,500,400);
+
+    for(i = 0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "status : object detected";
+        document.getElementById("number_of_objects").innerHTML = "number of objects : " + objects.length;
+
+        fill("red");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + "" + percent + "%",objects[i].x + 15,objects[i].y + 15);
+        noFill();
+        stroke("red");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
